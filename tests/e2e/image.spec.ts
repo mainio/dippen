377b3ccbce0c62ecf0b42f8621cc16afebc36850
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2025-08-08 17:13:46 +0300

    Add tests for dynamic links

--- tests/e2e/image.spec.ts
+++ tests/e2e/image.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from '@playwright/test';
-import { dragResize } from '../helpers/drag_utils';
+import { dragResize, deleteScreenShots, setContentHTML, elementClassList } from '../helpers/utils';
 
 test.describe('editor', () => {
-  let editor, toolbar, videoButton, imageButton, paragraphs, image, resizeDisplay, altBox;
+  let editor, editorContent, toolbar, videoButton, imageButton, paragraphs, image, resizeDisplay, altBox;
+
+  test.beforeAll(async() => {
+    await deleteScreenShots();
+  })
 
   test.beforeEach(async({ page }) => {
     await page.goto('/');
-    editor = page.locator('#editor'),
-    toolbar = editor.locator('.ql-toolbar'),
-    videoButton = toolbar.locator('button.ql-video'),
-    imageButton = toolbar.locator('button.ql-image'),
-    paragraphs = editor.locator('.ql-editor p'),
-    image = editor.locator('img[src="/images/600x400.png"]'),
-    resizeDisplay = page.locator('.ql-resize-display'), 
+    editor = page.locator('#editor');
+    editorContent = page.locator('.ql-editor')
+    toolbar = editor.locator('.ql-toolbar');
+    videoButton = toolbar.locator('button.ql-video');
+    imageButton = toolbar.locator('button.ql-image');
+    paragraphs = editor.locator('.ql-editor p');
+    image = editor.locator('img[src="/images/600x400.png"]');
+    resizeDisplay = page.locator('.ql-resize-display'); 
     altBox = page.locator('.ql-tooltip[data-mode="image-alt"]');
   });
 
@@ -23,7 +28,7 @@ test.describe('editor', () => {
       expect(toolbar).toBeVisible(),
       expect(videoButton).toBeVisible(),
       expect(imageButton).toBeVisible(),
-      expect(paragraphs).toHaveCount(2),
+      expect(paragraphs).toHaveCount(3),
       expect(image).toBeVisible(),
       expect(editor.locator('.ql-editor p:has-text("This is some content.")')).toHaveCount(1)
     ]);
@@ -201,11 +206,8 @@ test.describe('editor', () => {
     });
 
     test('Shift+Enter inserts a line break', async ({ page }) => {
-      const editor = page.locator('.ql-editor');
-      // const htmlBefore = await editor.innerHTML();
-      // console.log("BEFORE", htmlBefore)
       // Focus editor and type text
-      await editor.click();
+      await editorContent.click();
       await page.keyboard.type('Hello');
 
       // Shift+Enter should create a <br> in same paragraph
@@ -215,23 +217,159 @@ test.describe('editor', () => {
 
       await page.keyboard.type('World');
 
-      const html = await editor.innerHTML();
+      const html = await editorContent.innerHTML();
       expect(html).toContain('<p>Hello<br>World</p>');
     });
 
     test('Enter creates a new paragraph', async ({ page }) => {
-      const editor = page.locator('.ql-editor');
-
-      await editor.click();
+      await editorContent.click();
       await page.keyboard.type('Hello');
 
       await page.keyboard.press('Enter');
       await page.keyboard.type('World');
 
-      const html = await editor.innerHTML();
+      const html = await editorContent.innerHTML();
 
       expect(html).toContain('<p>Hello</p>');
       expect(html).toContain('<p>World</p>');
     });
   });
+
+  test.describe('dynamic-links', ()=> {
+    let link, defaultButton, newTabButton, linkEditDialogue, editLinkButton, removeButton, previewButton, editingDialogue
+    const html = `<p>This is a paragraph with a <a href="https://example.org" rel="noopener noreferrer" target="_blank">link</a></p>`;
+    test.beforeEach(async ({ page }) => {
+      await setContentHTML(html, page);
+
+      defaultButton = toolbar.locator('.ql-picker-label[data-label="Default"]');
+      newTabButton = toolbar.locator('.ql-picker-label[data-label="New tab"]');
+      link = editorContent.locator('a');
+      linkEditDialogue = editor.locator('.ql-flip');
+      editLinkButton = linkEditDialogue.locator('.ql-action');
+      removeButton = linkEditDialogue.locator('.ql-remove');
+      previewButton = linkEditDialogue.locator('.ql-preview');
+      editingDialogue = page.locator('.ql-editing[data-mode="link"]')
+    });
+
+    test('it toggles dynamic links when clicking the link', async({ page }) => {
+      await expect(link).toHaveCount(1);
+      await expect(link).toBeVisible();
+      
+      expect(defaultButton).toBeHidden();
+      expect(newTabButton).toBeHidden();
+      expect(linkEditDialogue).toBeHidden();
+      await link.click()
+      // opens the link edit and toggles the default and new tab buttons
+      expect(defaultButton).toBeVisible();
+      expect(newTabButton).toBeVisible();
+      expect(linkEditDialogue).toBeVisible();
+      expect(editLinkButton).toBeVisible();
+      expect(removeButton).toBeVisible();
+      expect(previewButton).toBeVisible();
+      const href = await previewButton.getAttribute('href');
+      expect(href).toContain('https://example.org');
+    })
+
+    test('edits the link url', async() => {
+      await link.click();
+      expect(linkEditDialogue).toBeVisible();
+      expect(editLinkButton).toBeVisible();
+      await editLinkButton.click(editLinkButton)
+      expect(editingDialogue).toBeVisible();
+      //change the link
+      const linkInput = editingDialogue.locator('input');
+      expect(linkInput).toBeVisible();
+      await linkInput.fill('https://another-link.org')
+      await editingDialogue.locator('a.ql-action').click()
+      const linkSrc = await link.getAttribute('href');
+      expect(linkSrc).toContain('https://another-link.org');
+    })
+
+    test.describe('new tab link', () => {
+      test('it toggles link new tab', async({ page }) => {
+        const target = await link.getAttribute('target');
+        expect(target).toEqual('_blank')
+        await link.click()
+        expect(newTabButton).toBeVisible();
+        await newTabButton.click();
+        const options = page.locator('.ql-dropdown .ql-picker-item');
+        await options.filter({ hasText: 'Link' }).click();
+        const editedLink = editorContent.locator('a');
+        const editedTarget = await editedLink.getAttribute('target');
+        expect(editedTarget).toEqual(null)
+      })
+    })
+
+    test.describe('dynamic class', () => {
+      test('it does not add any class by default', async() => {
+         const classes = await elementClassList(link)
+         expect(classes).toEqual([])
+      })
+      test('it changes to small button', async({ page }) => {
+         await link.click()
+        expect(defaultButton).toBeVisible();
+        await defaultButton.click();
+        const options = page.locator('.ql-dropdown .ql-picker-item');
+        await options.filter({ hasText: 'Small button' }).nth(0).click();
+        const updatedLink = editorContent.locator('a');
+        const classes = await elementClassList(updatedLink)
+        expect(classes).toEqual(expect.arrayContaining(["button", "primary", "small"]))
+      })
+
+      test('it changes to small hollow button', async({ page }) => {
+         await link.click()
+        expect(defaultButton).toBeVisible();
+        await defaultButton.click();
+        const options = page.locator('.ql-dropdown .ql-picker-item');
+        await options.filter({ hasText: 'Small button' }).nth(1).click();
+        const updatedLink = editorContent.locator('a');
+        const classes = await elementClassList(updatedLink)
+        expect(classes).toEqual(expect.arrayContaining(["button", "primary", "hollow", "small"]))
+      })
+
+      test('it changes to standard button', async({ page }) => {
+         await link.click()
+        expect(defaultButton).toBeVisible();
+        await defaultButton.click();
+        const options = page.locator('.ql-dropdown .ql-picker-item');
+        await options.filter({ hasText: 'Standard button' }).nth(0).click();
+        const updatedLink = editorContent.locator('a');
+        const classes = await elementClassList(updatedLink)
+        expect(classes).toEqual(expect.arrayContaining(["button", "primary"]))
+      })
+
+      test('it changes to standard hallow button', async({ page }) => {
+         await link.click()
+        expect(defaultButton).toBeVisible();
+        await defaultButton.click();
+        const options = page.locator('.ql-dropdown .ql-picker-item');
+        await options.filter({ hasText: 'Standard button' }).nth(1).click();
+        const updatedLink = editorContent.locator('a');
+        const classes = await elementClassList(updatedLink)
+        expect(classes).toEqual(expect.arrayContaining(["button", "primary", "hollow"]))
+      })
+
+      test('it changes to large button', async({ page }) => {
+         await link.click()
+        expect(defaultButton).toBeVisible();
+        await defaultButton.click();
+        const options = page.locator('.ql-dropdown .ql-picker-item');
+        await options.filter({ hasText: 'Large button' }).nth(0).click();
+        const updatedLink = editorContent.locator('a');
+        const classes = await elementClassList(updatedLink)
+        expect(classes).toEqual(expect.arrayContaining(["button", "primary", "large"]))
+      })
+
+      test('it changes to large hollow button', async({ page }) => {
+         await link.click()
+        expect(defaultButton).toBeVisible();
+        await defaultButton.click();
+        const options = page.locator('.ql-dropdown .ql-picker-item');
+        await options.filter({ hasText: 'Large button (hollow)' }).nth(0).click();
+        const updatedLink = editorContent.locator('a');
+        const classes = await elementClassList(updatedLink)
+        expect(classes).toEqual(expect.arrayContaining(["button", "primary", "hollow", "large"]))
+      })
+    })
+  })
 })
---
+++ tests/helpers/utils.ts
@@ -0,0 +1,47 @@
+import { Locator, Page } from '@playwright/test';
+import fs from 'fs';
+import path from 'path';
+
+export async function dragResize(
+  page: Page,
+  handle: Locator,
+  dragX: number,
+  dragY: number
+) {
+  const box = await handle.boundingBox();
+  if (!box) throw new Error('Cannot find bounding box for handle');
+
+  // Move mouse to the center of the handle
+  await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
+  await page.mouse.down();
+
+  // Drag relative to current position
+  await page.mouse.move(box.x + box.width / 2 + dragX, box.y + box.height / 2 + dragY);
+
+  await page.mouse.up();
+}
+
+export async function deleteScreenShots() {
+  const screenShotPath = path.join(rootPath, 'screenshots')
+  if (fs.existsSync(screenShotPath)) {
+    const files = fs.readdirSync(screenShotPath);
+    for (const file of files) {
+      fs.unlinkSync(path.join(screenShotPath, file));
+    }
+  }
+}
+
+export async function setContentHTML(htmlString: string, page) {
+  await page.evaluate((html) => {
+    const editor = (window as any).editor;
+    editor.setContents([]);
+    editor.clipboard.dangerouslyPasteHTML(html);
+  }, htmlString);
+}
+
+export async function elementClassList(element) {
+  const classList = await element.getAttribute('class');
+  return classList ? classList.split(/\s+/) : [];
+}
+
+const rootPath = path.resolve(__dirname, '../..');
